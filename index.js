97dec1c5f5c8f380cdcbacff4f7e867007086da2
koemnak <heatkimnak@gmail.com>
2023-03-21 17:12:40 +0700

    commited

--- index.js
+++ index.js
@@ -1,34 +1,10 @@
-window.onscroll = function () {
-  myFunction();
-};
-
-var header = document.getElementById("mynav-bar");
-var sticky = header.offsetTop;
-
-function myFunction() {
-  if (window.pageYOffset > sticky) {
-    header.classList.add("sticky");
-  } else {
-    header.classList.remove("sticky");
-  }
-}
-
-<script src="https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.js"></script>;
-const swiper = new Swiper(".swiper", {
-  autoplay: {
-    delay: 5000,
-
-    disableOnInteration: false,
-  },
-  loop: true,
-  // If we need pagination
-  pagination: {
-    el: ".swiper-pagination",
-  },
-
-  // Navigation arrows
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
+$(".custom-carousel").owlCarousel({
+  autoWidth: true,
+  loop: true
+});
+$(document).ready(function () {
+  $(".custom-carousel .item").click(function () {
+    $(".custom-carousel .item").not($(this)).removeClass("active");
+    $(this).toggleClass("active");
+  });
 });
