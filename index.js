32ecee1363dad760dda2ebd4c58f815b154d101e
koemnak <heatkimnak@gmail.com>
2023-03-28 23:47:48 +0700

    commited

--- index.js
+++ index.js
@@ -1,10 +1,5 @@
-$(".custom-carousel").owlCarousel({
-  autoWidth: true,
-  loop: true
-});
-$(document).ready(function () {
-  $(".custom-carousel .item").click(function () {
-    $(".custom-carousel .item").not($(this)).removeClass("active");
-    $(this).toggleClass("active");
-  });
-});
+const btn= document.getElementById("add_to_card")
+function myFunction() {
+  btn.style.Color="red";
+  
+}
