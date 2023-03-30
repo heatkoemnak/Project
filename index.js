5b23e3358fac1a5e84004c5c3a317066307c0266
koemnak <heatkimnak@gmail.com>
2023-03-30 18:12:50 +0700

    commited

--- index.js
+++ index.js
@@ -9,21 +9,6 @@ function login(){
 function signup(){
   window.open("signup.html");
 }
-// function signin(){
- 
-//   const log =document.getElementById("login-btn");
-//   const sign = document.getElementById("signup-btn");
-//   sign.style.display = "none";
-//   log.style.display = "block";
-// }
-// function signup(){
-//   const sign = document.getElementById("signup-btn");
-//   const log =document.getElementById("login-btn");
-//   sign.style.display = "block";
-//   log.style.display = "none";
-
-  
-// }
 
 
 
