ba01d62012fd54d14f2919b114a7b725fb87e4f3
koemnak <heatkimnak@gmail.com>
2023-04-02 19:59:23 +0700

    commited

--- index.js
+++ index.js
@@ -38,8 +38,8 @@ var slideshows = document.querySelectorAll('[data-component="slideshow"]');
 
     }, time);
   }
-  function Product(){
-    window.open("homepage.html");
+  function home(){
+    window.open("home.html");
   }
 
 
